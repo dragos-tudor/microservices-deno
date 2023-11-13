a84cf3486c60edc04e2c5a25538237fa9fd045de
Dragos Tudor <dragos.tudor@gmail.com>
2023-11-13 21:12:36 +0000

    fix: save sent notification message after email is sent.

--- notifications-api/notifications-handlers/send-notification/sending.js
+++ notifications-api/notifications-handlers/send-notification/sending.js
@@ -1,7 +1,7 @@
 import { logInfo } from "../../../std-modules/std-logging/mod.js";
 import { createApiTrace } from "../../../std-modules/std-tracing/mod.js";
 import { getApiName } from "../../notifications-config/mod.js";
-import { createDbMessage, deleteDbMessage, generateId, getDbMessageByType, insertDbMessage } from "../../notifications-database/mod.js"
+import { createDbMessage, generateId, getDbMessageByType, insertDbMessage } from "../../notifications-database/mod.js"
 import { deserializeEvent, EventTypes } from "../../notifications-domain/mod.js"
 
 export const sendNotificationv1 = async (notificationRegistered, apiContext) =>
@@ -11,11 +11,9 @@ export const sendNotificationv1 = async (notificationRegistered, apiContext) =>
   if(await getDbMessageByType(EventTypes.notificationSent, messageId, db)) return
 
   const {emailContent} = deserializeEvent(payload)
-  const notificationSent = createDbMessage(generateId(), EventTypes.notificationSent, "", messageId, traceId, getUtc())
+  logInfo("notification sent", emailContent, createApiTrace(getApiName(apiConfig), traceId)) //simulate send to external services
+
+  const notificationSent = createDbMessage(generateId(), EventTypes.notificationSent, "", messageId, traceId, getUtc(), 0)
   await insertDbMessage(notificationSent, db)
-
-  logInfo("notification sent", emailContent, createApiTrace(getApiName(apiConfig), traceId))
-
-  await deleteDbMessage(notificationSent.messageId, db)
   return notificationSent
 }
