961777d12ede1b82ff3e0d688965e54d8d3caa6f
Dragos Tudor <dragos.tudor@gmail.com>
2023-12-10 14:13:47 +0000

    chore: update certificates script.

--- certificates.sh
+++ certificates.sh
@@ -1,2 +1,7 @@
+set -e
+
+mkcert
+echo "export DENO_CERT=$(mkcert -CAROOT)/rootCA.pem" >> $HOME/.bashrc
+
 [ ! -d "./.certificates" ] && mkdir ./.certificates
 [ ! -f "./.certificates/localhost.pem" ] && cd ./.certificates && mkcert localhost
